e0a7bdbfa6624ba01853cacdab1d94ef1642f1f3
Jesus Garcia <zerovamp@hotmail.com>
2018-06-27 03:47:06 +0000

    Better code structure ot variables.

--- tests/Unit/RepositoryTest.php
+++ tests/Unit/RepositoryTest.php
@@ -13,40 +13,34 @@ class RepositoryTest extends TestCase
   use RefreshDatabase;
 
   /**
+   * The data with the repository are filled.
+   *
+   * @var array
+   */
+  private $_data = [
+    'node_id' => 'MDEwOlJlcG9zaXRvcnk4NzA4NTkyMA',
+    'name' => 'polygons',
+    'full_name' => 'githubtraining/polygons',
+    'html_url' => 'https://github.com/githubtraining/polygons',
+    'url' => 'https://api.github.com/repos/githubtraining/polygons',
+    'commits_url' => 'https://api.github.com/repos/githubtraining/polygons/commits{/sha}',
+    'git_commits_url' => 'https://api.github.com/repos/githubtraining/polygons/git/commits{/sha}',
+    'repo_created_at' => '2017-04-03T14:51:26Z',
+    'repo_updated_at' => '2018-04-27T00:55:12Z',
+    'repo_pushed_at' => '2018-04-03T14:11:51Z',
+  ];
+
+  /**
    * A test of database correct insert with hardcoded data.
    *
    * @return void
    */
   public function testDatabaseTest()
   {
-    $data = [
-      'node_id' => 'MDEwOlJlcG9zaXRvcnk4NzA4NTkyMA',
-      'name' => 'polygons',
-      'full_name' => 'githubtraining/polygons',
-      'html_url' => 'https://github.com/githubtraining/polygons',
-      'url' => 'https://api.github.com/repos/githubtraining/polygons',
-      'commits_url' => 'https://api.github.com/repos/githubtraining/polygons/commits{/sha}',
-      'git_commits_url' => 'https://api.github.com/repos/githubtraining/polygons/git/commits{/sha}',
-      'repo_created_at' => '2017-04-03T14:51:26Z',
-      'repo_updated_at' => '2018-04-27T00:55:12Z',
-      'repo_pushed_at' => '2018-04-03T14:11:51Z',
-    ];
-
-    $repository = new Repository($data);
+    $repository = new Repository($this->_data);
     $repository->save();
 
-    $this->assertDatabaseHas('repositories', [
-      'node_id' => 'MDEwOlJlcG9zaXRvcnk4NzA4NTkyMA',
-      'name' => 'polygons',
-      'full_name' => 'githubtraining/polygons',
-      'html_url' => 'https://github.com/githubtraining/polygons',
-      'url' => 'https://api.github.com/repos/githubtraining/polygons',
-      'commits_url' => 'https://api.github.com/repos/githubtraining/polygons/commits{/sha}',
-      'git_commits_url' => 'https://api.github.com/repos/githubtraining/polygons/git/commits{/sha}',
-      'repo_created_at' => '2017-04-03T14:51:26Z',
-      'repo_updated_at' => '2018-04-27T00:55:12Z',
-      'repo_pushed_at' => '2018-04-03T14:11:51Z'
-    ]);
+    $this->assertDatabaseHas('repositories', $this->_data);
   }
 
   /**
@@ -56,31 +50,9 @@ class RepositoryTest extends TestCase
    */
   public function testModelFactoryTest()
   {
-    $repository = factory(\App\Repository::class)->make([
-      'node_id' => 'MDEwOlJlcG9zaXRvcnk4NzA4NTkyMA',
-      'name' => 'polygons',
-      'full_name' => 'githubtraining/polygons',
-      'html_url' => 'https://github.com/githubtraining/polygons',
-      'url' => 'https://api.github.com/repos/githubtraining/polygons',
-      'commits_url' => 'https://api.github.com/repos/githubtraining/polygons/commits{/sha}',
-      'git_commits_url' => 'https://api.github.com/repos/githubtraining/polygons/git/commits{/sha}',
-      'repo_created_at' => '2017-04-03T14:51:26Z',
-      'repo_updated_at' => '2018-04-27T00:55:12Z',
-      'repo_pushed_at' => '2018-04-03T14:11:51Z',
-    ]);
+    $repository = factory(\App\Repository::class)->make($this->_data);
     $repository->save();
 
-    $this->assertDatabaseHas('repositories', [
-      'node_id' => 'MDEwOlJlcG9zaXRvcnk4NzA4NTkyMA',
-      'name' => 'polygons',
-      'full_name' => 'githubtraining/polygons',
-      'html_url' => 'https://github.com/githubtraining/polygons',
-      'url' => 'https://api.github.com/repos/githubtraining/polygons',
-      'commits_url' => 'https://api.github.com/repos/githubtraining/polygons/commits{/sha}',
-      'git_commits_url' => 'https://api.github.com/repos/githubtraining/polygons/git/commits{/sha}',
-      'repo_created_at' => '2017-04-03T14:51:26Z',
-      'repo_updated_at' => '2018-04-27T00:55:12Z',
-      'repo_pushed_at' => '2018-04-03T14:11:51Z'
-    ]);
+    $this->assertDatabaseHas('repositories', $this->_data);
   }
 }
